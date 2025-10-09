c0b79be4488a09260c29ea10aee2a1f233ce0270
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-10-09 14:15:55 +0400

    Updated Homepage.tsx

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -48,7 +48,7 @@ export function Homepage({ onGetStarted }: Props) {
               
               <div className="mt-8 flex items-center justify-center lg:justify-start space-x-6 text-sm text-gray-600">
                 <div className="flex items-center space-x-2">
-                  <CheckCircle className="w-5 h-5 text-green-600" />
+                  
                   
                 </div>
                 <div className="flex items-center space-x-2">
