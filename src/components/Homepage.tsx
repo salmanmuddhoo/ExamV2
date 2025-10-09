c19af578baed387fce2a447d3515bbf6e7da4984
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-10-09 14:16:09 +0400

    Updated Homepage.tsx

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -52,8 +52,6 @@ export function Homepage({ onGetStarted }: Props) {
                   
                 </div>
                 <div className="flex items-center space-x-2">
-                  <CheckCircle className="w-5 h-5 text-green-600" />
-                  <span>Free forever</span>
                 </div>
               </div>
             </div>
