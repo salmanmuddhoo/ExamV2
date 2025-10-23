af803326932fcfe366b38f62c0ec793a47e1506d
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-10-23 09:13:34 +0400

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -72,7 +72,7 @@ export function Navbar({ onNavigateHome, onNavigateAdmin, onNavigateLogin, onNav
   <img
     src="/assets/logo.png"
     alt="Exam Study Assistant Logo"
-    className="w-8 h-16 object-contain"
+    className="w-16 h-16 object-contain"
   />
   <span className="font-semibold text-lg hidden sm:inline">Exam Study Assistant</span>
   <span className="font-semibold text-lg sm:hidden">ESA</span>
